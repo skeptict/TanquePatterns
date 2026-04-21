335c7fd070d116697ebb52b0e26e61a481ce99e1
skeptict <skeptict@users.noreply.github.com>
2026-04-21 11:00:42 -0400

    Initial Commit

---
+++ TanquePatterns/TanquePatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TanquePatternsApp.swift
+//  TanquePatterns
+//
+//  Created by skeptict on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct TanquePatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
